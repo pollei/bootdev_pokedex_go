d888c8cf46f9e0d0d5aae2e5b60d980f6f2a80ab
pollei <stephen.pollei@gmail.com>
2025-08-01 20:22:44 -0700

    cache passes first tests

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -10,27 +10,67 @@ type CacheEntry struct {
 	val       []byte
 }
 type Cache struct {
-	Entries map[string]CacheEntry
-	mutx    sync.Mutex
+	Entries  map[string]CacheEntry
+	tick     *time.Ticker
+	interval time.Duration
+	done     chan bool
+	mutx     sync.Mutex
 }
 
-func NewCache(interval time.Duration) Cache {
-	return Cache{}
+func NewCache(interval time.Duration) *Cache {
+	ret := Cache{}
+	ret.Entries = make(map[string]CacheEntry, 50)
+	ret.interval = interval
+	ret.tick = time.NewTicker(interval)
+	ret.done = make(chan bool)
+	go ret.ReapLoop()
+	return &ret
 }
 
-func (c Cache) Add(key string, val []byte) {
+func (c *Cache) Add(key string, val []byte) {
 	c.mutx.Lock()
 	defer c.mutx.Unlock()
-
+	//elem, ok := c.Entries[key]
+	c.Entries[key] = CacheEntry{time.Now(), val}
 }
-func (c Cache) Get(key string) ([]byte, bool) {
-	ret := []byte{}
+func (c *Cache) Get(key string) ([]byte, bool) {
+	//ret := []byte{}
 	c.mutx.Lock()
 	defer c.mutx.Unlock()
-	return ret, false
+	elem, ok := c.Entries[key]
+	if ok {
+		elem.createdAt = time.Now()
+		return elem.val, true
+	}
+	return nil, false
 
 }
 
-func (c Cache) ReapLoop() {
+func (c *Cache) reap() {
+	c.mutx.Lock()
+	defer c.mutx.Unlock()
+	t := time.Now()
+	var delList []string
+	//expTime := t.Sub(c.interval)
+	for keyName, entry := range c.Entries {
+		expTime := entry.createdAt.Add(c.interval)
+		if t.After(expTime) {
+			delList = append(delList, keyName)
+		}
+	}
+	for _, keyName := range delList {
+		delete(c.Entries, keyName)
+	}
+}
+
+func (c *Cache) ReapLoop() {
+	for {
+		select {
+		case <-c.done:
+			return
+		case <-c.tick.C:
+			c.reap()
+		}
+	}
 
 }
